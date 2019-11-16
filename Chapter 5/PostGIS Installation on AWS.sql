872fae42c33c1ea3d7d08b530f1e870dd0d17373
lequanha <nlpsr.ha@gmail.com>
2019-11-16 01:12:24 -0700

    Create Chapter 5

---
+++ Chapter 5/PostGIS Installation on AWS.sql
@@ -0,0 +1,11 @@
+CREATE EXTENSION postgis;
+
+CREATE EXTENSION postgis_topology;
+
+CREATE EXTENSION fuzzystrmatch;
+
+CREATE EXTENSION postgis_tiger_geocoder;
+
+CREATE EXTENSION address_standardizer;
+
+SELECT name, default_version,installed_version FROM pg_available_extensions WHERE name LIKE 'postgis%' or name LIKE 'address%';
