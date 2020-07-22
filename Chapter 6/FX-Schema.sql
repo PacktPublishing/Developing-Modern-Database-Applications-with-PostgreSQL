9364263f12b4375c46fb4b4fd08e2a0a097dacbf
lequanha <nlpsr.ha@gmail.com>
2020-07-21 18:36:45 -0600

    Update FX-Schema.sql

--- Chapter 6/FX-Schema.sql
+++ Chapter 6/FX-Schema.sql
@@ -11,6 +11,6 @@ CREATE TABLE "ExchangeRates"
     highestrate double precision,
     lowestrate double precision,
     closingrate double precision,
-    currencycode character varying(3),
-    PRIMARY KEY ("time", currencycode)
+    currencycode character varying(3) REFERENCES public."Currencies" (currencycode) ON DELETE CASCADE,
+	PRIMARY KEY ("time", currencycode)
 );
