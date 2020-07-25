5b9ac08076c33e625ce86c86556d479fac96cb80
lequanha <nlpsr.ha@gmail.com>
2020-07-24 19:52:11 -0600

    Delete Currencies.sql

--- Chapter 6/Currencies.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO "Currencies" (currencycode, currencyname) 
-VALUES ('USD', 'United States Dollar'),
-('CAD', 'Canadian Dollar'),
-('GBP', 'British Pound Sterling'),
-('EUR', 'Euro (European Monetary Unit)');
