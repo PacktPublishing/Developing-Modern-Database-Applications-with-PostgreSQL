69cad9f1c7f6af11061c4f0a9557f2f4f3d520b1
lequanha <nlpsr.ha@gmail.com>
2020-07-24 20:00:07 -0600

    Delete Script 6 - Add a Trusted User.sh

--- Chapter 6/Script 6 - Add a Trusted User.sh
+++
@@ -1,6 +0,0 @@
-create role atm_user nologin;
-grant atm_user to dba;
-
-grant usage on schema public to atm_user;
-grant all on public."ATM locations" to atm_user;
-grant usage, select on sequence public."ATM locations_ID_seq1" to atm_user;
