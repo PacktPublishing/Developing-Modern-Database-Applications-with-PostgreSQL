2de11eb1eb0018c46ff42b9968c20bf3440088ac
lequanha <nlpsr.ha@gmail.com>
2020-07-24 20:00:02 -0600

    Delete Script 4 - Configuration file.sh

--- Chapter 6/Script 4 - Configuration file.sh
+++
@@ -1,9 +0,0 @@
-[root@ip-172-31-95-213 src]# mkdir postgrest
-[root@ip-172-31-95-213 src]# cd postgrest
-[root@ip-172-31-95-213 postgrest]# vi tutorial.conf
-------------------------------------------
-db-uri = "postgres://dba:bookdemo@atm.c3qdepivtce8.us-east-1.rds.amazonaws.com/atm"
-db-schema = "public"
-db-anon-role = "web_anon"
-------------------------------------------
-[root@ip-172-31-95-213 postgrest]# postgrest tutorial.conf
